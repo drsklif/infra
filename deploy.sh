136947b8d51dec0fbfff2f84742f6a41e2e08517
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-09-03 19:43:05 +0700

    Homework 06 part 2, fix 4 for startup-script - wrong bundle path

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ echo 1. copy app
 git clone https://github.com/Artemmkin/reddit.git
 
 echo 2. installing app
+source /usr/local/rvm/gems/ruby-2.4.1/bin/bundle
 cd reddit && bundle install
 
 echo 3. launch app server
