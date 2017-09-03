151519b1bba600f5433c232bffb3ee901dddac41
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-09-03 20:39:55 +0700

    Homework 06 part 2, fix 8 for startup-script - задолбали эти пляски с путями

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,11 @@ echo 1. copy app
 git clone https://github.com/Artemmkin/reddit.git
 
 echo 2. installing app
+echo $PATH
 sh /usr/local/rvm/environments/default
+echo $PATH
+export PATH="$PATH:/usr/local/rvm/gems/ruby-2.4.1/bin"
+echo $PATH
 cd reddit && bundle install
 
 echo 3. launch app server
