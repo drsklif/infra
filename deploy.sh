bd75231d10337e516c8523366696a131238c09b9
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-09-03 20:55:44 +0700

    Homework 06 part 2, fix 9 for startup-script - one more time

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,14 @@ echo 1. copy app
 git clone https://github.com/Artemmkin/reddit.git
 
 echo 2. installing app
-echo $PATH
-sh /usr/local/rvm/environments/default
-echo $PATH
-export PATH="$PATH:/usr/local/rvm/gems/ruby-2.4.1/bin"
+export PATH="/usr/local/rvm/gems/ruby-2.4.1/bin:/usr/local/rvm/gems/ruby-2.4.1@global/bin:/usr/local/rvm/rubies/ruby-2.4.1/bin:$PATH"
+export GEM_HOME='/usr/local/rvm/gems/ruby-2.4.1'
+export GEM_PATH='/usr/local/rvm/gems/ruby-2.4.1:/usr/local/rvm/gems/ruby-2.4.1@global'
+export MY_RUBY_HOME='/usr/local/rvm/rubies/ruby-2.4.1'
+export IRBRC='/usr/local/rvm/rubies/ruby-2.4.1/.irbrc'
+unset MAGLEV_HOME
+unset RBXOPT
+export RUBY_VERSION='ruby-2.4.1'
 echo $PATH
 cd reddit && bundle install
 
