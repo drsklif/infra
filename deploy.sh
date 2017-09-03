5d28c3ed8a464173149150e9aa9b30567cf2b262
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-09-03 20:25:14 +0700

    Homework 06 part 2, fix 6 for startup-script - export ruby paths

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo 1. copy app
 git clone https://github.com/Artemmkin/reddit.git
 
 echo 2. installing app
-export PATH=$PATH:/usr/local/rvm/gems/ruby-2.4.1/bin
+sh /usr/local/rvm/environments/ruby-2.4.1@global
 cd reddit && bundle install
 
 echo 3. launch app server
