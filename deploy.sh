af86e94cb7199b7d948eff049de0eeb19c3053bb
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-09-03 20:34:58 +0700

    Homework 06 part 2, fix 7 for startup-script - apply default environment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo 1. copy app
 git clone https://github.com/Artemmkin/reddit.git
 
 echo 2. installing app
-sh /usr/local/rvm/environments/ruby-2.4.1@global
+sh /usr/local/rvm/environments/default
 cd reddit && bundle install
 
 echo 3. launch app server
