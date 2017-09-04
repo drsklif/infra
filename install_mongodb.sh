cdebeb12631edcd8ecc8e08c8602bbb0cf8937ff
Ildyakov Alexander <a.ildjakov@yandex.ru>
2017-09-04 21:56:43 +0700

    проверка без переменных окружения

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,15 +3,15 @@
 echo Part 2 - Installing MongoDB
 
 echo 1. adding keys and repository:
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 
 echo 2. updating system and installing mongodb
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-get update
+apt-get install -y mongodb-org
 
 echo 3. launch MongoDB
-sudo systemctl start mongod
+systemctl start mongod
 
 echo 4. setting autorun
-sudo systemctl enable mongod
+systemctl enable mongod
--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://github.com/drsklif/infra/raw/config-scripts/install_ruby.sh | bash -s
+su - appuser -c "curl -sL https://github.com/drsklif/infra/raw/config-scripts/install_ruby.sh | bash -s"
 curl -sL https://github.com/drsklif/infra/raw/config-scripts/install_mongodb.sh | bash -s
-curl -sL https://github.com/drsklif/infra/raw/config-scripts/deploy.sh | bash -s
+su - appuser -c "curl -sL https://github.com/drsklif/infra/raw/config-scripts/deploy.sh | bash -s"
